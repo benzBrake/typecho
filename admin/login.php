b6a8df1d24445038917cc3c24f294bc18a31401e
Ryan Lieu <github-benzBrake@woai.ru>
2026-01-20 03:02:20 +0800

    feat: add a message indicating that the email address can also be used for logging in.

--- admin/login.php
+++ admin/login.php
@@ -16,8 +16,8 @@ include 'header.php';
         <h1><a href="https://typecho.org" class="i-logo">Typecho</a></h1>
         <form action="<?php $options->loginAction(); ?>" method="post" name="login" role="form">
             <p>
-                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
-                <input type="text" id="name" name="name" value="<?php echo $rememberName; ?>" placeholder="<?php _e('用户名'); ?>" class="text-l w-100" autofocus />
+                <label for="name" class="sr-only"><?php _e('用户名或邮箱'); ?></label>
+                <input type="text" id="name" name="name" value="<?php echo $rememberName; ?>" placeholder="<?php _e('用户名或邮箱'); ?>" class="text-l w-100" autofocus />
             </p>
             <p>
                 <label for="password" class="sr-only"><?php _e('密码'); ?></label>
@@ -29,27 +29,27 @@ include 'header.php';
             </p>
             <p>
                 <label for="remember">
-                    <input<?php if(\Typecho\Cookie::get('__typecho_remember_remember')): ?> checked<?php endif; ?> type="checkbox" name="remember" class="checkbox" value="1" id="remember" /> <?php _e('下次自动登录'); ?>
+                    <input<?php if (\Typecho\Cookie::get('__typecho_remember_remember')): ?> checked<?php endif; ?> type="checkbox" name="remember" class="checkbox" value="1" id="remember" /> <?php _e('下次自动登录'); ?>
                 </label>
             </p>
         </form>
-        
+
         <p class="more-link">
             <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
-            <?php if($options->allowRegister): ?>
-            &bull;
-            <a href="<?php $options->registerUrl(); ?>"><?php _e('用户注册'); ?></a>
+            <?php if ($options->allowRegister): ?>
+                &bull;
+                <a href="<?php $options->registerUrl(); ?>"><?php _e('用户注册'); ?></a>
             <?php endif; ?>
         </p>
     </div>
 </div>
-<?php 
+<?php
 include 'common-js.php';
 ?>
 <script>
-$(document).ready(function () {
-    $('#name').focus();
-});
+    $(document).ready(function() {
+        $('#name').focus();
+    });
 </script>
 <?php
 include 'footer.php';
